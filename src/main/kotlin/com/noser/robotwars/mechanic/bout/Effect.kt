7614f127ac72f9159f63371029736625dc0786e7
Thomas Chassaing <Thomas.Chassaing@gmail.com>
2019-05-05 13:36:39 +0200

    Add applyTo to Effect so we can let the Effect be applied to a Robot instead of doing a when(effect)

--- src/main/kotlin/com/noser/robotwars/mechanic/bout/Effect.kt
+++ src/main/kotlin/com/noser/robotwars/mechanic/bout/Effect.kt
@@ -2,11 +2,23 @@ package com.noser.robotwars.mechanic.bout
 
 sealed class Effect {
 
-    class Fire : Effect()
+    abstract fun applyTo(robot : Robot) : Robot
 
-    class Burnable : Effect()
+    class Fire : Effect() {
+        override fun applyTo(robot: Robot): Robot {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class Burnable : Effect() {
+        override fun applyTo(robot: Robot) = robot
+    }
 
     class Energy(val amount : Int) : Effect() {
+
+        override fun applyTo(robot: Robot): Robot {
+            TODO()
+        }
 
         init {
             check(amount > 0) {
